3c51ace43a774c3711706d2edf9d1016eed03223
Jeff Reiner <theundeadghost@gmail.com>
2021-08-20 11:24:58 +0200

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export default function omit<
+  T extends object,
+  K extends Extract<keyof T, string>
+>(obj: T, ...keys: K[]): Omit<T, K> {
+  let ret: any = {};
+  const exclude: Set<string> = new Set(keys);
+
+  for (let key in obj) {
+    if (!exclude.has(key)) {
+      ret[key] = obj[key];
+    }
+  }
+
+  return ret;
+}
---
+++ test/omit.test.ts
@@ -0,0 +1,27 @@
+import omit from '../src';
+
+describe('omits keys', () => {
+  it('return without a key', () => {
+    const source = {
+      foo: 'foo',
+      bar: 'bar',
+    };
+
+    const obj = omit(source, 'foo');
+
+    expect(obj).not.toHaveProperty('foo');
+  });
+
+  it('return without multiple keys', () => {
+    const source = {
+      foo: 'foo',
+      bar: 'bar',
+      fizz: 'fizz',
+      buzz: 'buzz',
+    };
+
+    const obj = omit(source, 'bar', 'fizz', 'buzz');
+
+    expect(obj).not.toHaveProperty(['bar', 'fizz', 'buzz']);
+  });
+});
