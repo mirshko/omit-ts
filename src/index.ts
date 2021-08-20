65d5b8ca79a156e3b75f37dbff46314dc7c8a31d
Jeff Reiner <theundeadghost@gmail.com>
2021-08-20 12:20:32 +0200

    Improvements

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
-interface OmitInterface {
-  <T extends object, K extends [...(keyof T)[]]>(obj: T, keys: K): {
-    [K2 in Exclude<keyof T, K[number]>]: T[K2];
-  };
-}
-
-export const omit: OmitInterface = (obj, keys) => {
+export default function omit<T extends object, K extends [...(keyof T)[]]>(
+  obj: T,
+  keys: K
+): {
+  [K2 in Exclude<keyof T, K[number]>]: T[K2];
+} {
   const ret = {} as {
     [K in keyof typeof obj]: typeof obj[K];
   };
 
   let key: keyof typeof obj;
 
-  for (key in obj) {
-    if (!keys.includes(key)) ret[key] = obj[key];
-  }
+  for (key in obj) if (!keys.includes(key)) ret[key] = obj[key];
 
   return ret;
-};
+}
